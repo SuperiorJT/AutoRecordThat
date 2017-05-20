e25ad0340cb10398914e546e9f46b52af05c65f7
Justin Miller <superiorjt@gmail.com>
2017-05-20 02:26:50 -0400

    Create WindowManager class and bind window titles to a combobox

--- AutoRecordThat/MainWindow.xaml.cs
+++ AutoRecordThat/MainWindow.xaml.cs
@@ -20,9 +20,57 @@ namespace AutoRecordThat
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        Dictionary<string, IntPtr> windowMap = new Dictionary<string, IntPtr>();
+        public List<string> WindowTitles = new List<string>();
+
+        IntPtr selectedWindow = new IntPtr();
+        string selectedWindowTitle = "";
+
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = this;
+            UpdateWindowList();
+            cbWindowTitles.SelectedIndex = 0;
+            selectedWindowTitle = WindowTitles[0];
+            windowMap.TryGetValue(WindowTitles[0], out selectedWindow);
+        }
+
+        private void UpdateWindowList()
+        {
+            List<IntPtr> windows = WindowManager.FilterWindows();
+            windows.ForEach(AddWindowToMap);
+            WindowTitles = windowMap.Keys.ToList();
+            cbWindowTitles.ItemsSource = WindowTitles;
+            if (selectedWindowTitle != "")
+            {
+                windowMap.TryGetValue(selectedWindowTitle, out selectedWindow);
+            }
+        }
+
+        private void AddWindowToMap(IntPtr hWnd)
+        {
+            int size = WindowManager.GetWindowTextLength(hWnd);
+            StringBuilder sb = new StringBuilder(size++);
+            WindowManager.GetWindowText(hWnd, sb, size);
+            int count = 0;
+            string title = "[" + count + "] - " + sb.ToString();
+            bool isInMap = windowMap.ContainsKey(title);
+            while (isInMap)
+            {
+                count++;
+                title = "[" + count + "] - " + sb.ToString();
+                isInMap = windowMap.ContainsKey(title);
+            }
+            windowMap.Add(title, hWnd);
+        }
+
+        void UpdateSelectedWindow(object sender, SelectionChangedEventArgs args)
+        {
+            string key = (string) cbWindowTitles.SelectedValue;
+            selectedWindowTitle = key;
+            windowMap.TryGetValue(key, out selectedWindow);
         }
     }
 }
---
+++ AutoRecordThat/WindowManager.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace AutoRecordThat
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct Rect
+    {
+        public int Left;
+
+        public int Top;
+
+        public int Right;
+
+        public int Bottom;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    internal struct MonitorInfoEx
+    {
+        public int cbSize;
+        public Rect rcMonitor;
+        public Rect rcWork;
+        public UInt32 dwFlags;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 32)] public string szDeviceName;
+    }
+
+    class WindowManager
+    {
+        [DllImport("user32.dll")]
+        protected static extern bool GetWindowRect(IntPtr hWnd, out Rect lpRect);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder strText, int maxCount);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        public static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        protected static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        protected static extern bool IsWindowVisible(IntPtr hWnd);
+
+        [DllImport("User32")]
+        protected static extern IntPtr MonitorFromWindow(IntPtr hWnd, int dwFlags);
+
+        [DllImport("user32", EntryPoint = "GetMonitorInfo", CharSet = CharSet.Auto,
+            SetLastError = true)]
+        internal static extern bool GetMonitorInfoEx(IntPtr hMonitor, ref MonitorInfoEx lpmi);
+
+        protected static bool EnumTheWindows(IntPtr hWnd, IntPtr lParam)
+        {
+            const int MONITOR_DEFAULTTOPRIMARY = 1;
+            var mi = new MonitorInfoEx();
+            mi.cbSize = Marshal.SizeOf(mi);
+            GetMonitorInfoEx(MonitorFromWindow(hWnd, MONITOR_DEFAULTTOPRIMARY), ref mi);
+
+            int size = GetWindowTextLength(hWnd);
+            if (size++ > 0 && IsWindowVisible(hWnd))
+            {
+                GCHandle gcHandle = (GCHandle) lParam;
+                List<IntPtr> windows = (gcHandle.Target as List<IntPtr>);
+
+                windows.Add(hWnd);
+            }
+            
+            return true;
+        }
+
+        public static bool FilterFullScreenWindows(IntPtr hWnd, MonitorInfoEx mi)
+        {
+            bool isFullscreen = false;
+            Rect appBounds;
+            GetWindowRect(hWnd, out appBounds);
+
+            int windowHeight = appBounds.Right - appBounds.Left;
+            int windowWidth = appBounds.Bottom - appBounds.Top;
+
+            int monitorHeight = mi.rcMonitor.Right - mi.rcMonitor.Left;
+            int monitorWidth = mi.rcMonitor.Bottom - mi.rcMonitor.Top;
+
+            isFullscreen = (windowHeight == monitorHeight) && (windowWidth == monitorWidth);
+
+            return isFullscreen;
+        }
+
+        public static List<IntPtr> FilterWindows()
+        {
+            List<IntPtr> windows = new List<IntPtr>();
+            GCHandle gcHandle = GCHandle.Alloc(windows);
+            EnumWindows(EnumTheWindows, (IntPtr) gcHandle);
+            return windows;
+        }
+
+        public delegate bool WindowFilter(IntPtr hWnd, MonitorInfoEx mi);
+
+        protected delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+    }
+}
